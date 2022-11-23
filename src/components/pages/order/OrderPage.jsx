6129ad1661519f2a039b52bad2cbe0a1533857e0
stephane.malho <stepahen.malho@gmail.com>
2022-11-23 10:59:38 +0100

    ♻️ F03 : OrderPage > create NavBar component

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
-import { BsPersonCircle } from "react-icons/bs";
 
-import Logo from "../../reusableUI/Logo";
-import LogoImg from "../../../assets/images/logo-orange.jpg";
-import { theme } from "../../../assets/theme/index";
-import UserProfile from "../../reusableUI/UserProfile";
+import NavBar from "../../reusableUI/NavBar";
 
 function OrderPage() {
   const navigate = useNavigate();
@@ -20,13 +16,7 @@ function OrderPage() {
   return (
     <OrderPageStyled>
       <main>
-        <nav>
-            <Logo
-              props={<img onClick={() => {window.location.reload()}} src={LogoImg} alt="dessin d'un burger" />}
-              className={"orderLogo"}
-            />
-            <UserProfile userName={userName} onClick={handleLogin} className={"userBox"} Icon={<BsPersonCircle className="icon" />}/>
-        </nav>
+        <NavBar userName={userName} handleLogin={handleLogin} />
       </main>
     </OrderPageStyled>
   );
@@ -51,36 +41,6 @@ const OrderPageStyled = styled.div`
     width: 96%;
     border-radius: 10px;
     box-shadow: 0px 0px 16px 0px grey inset;
-    nav {
-      position: relative;
-      background-color: white;
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-      flex-wrap: nowrap;
-      height: 80px;
-      border-radius: 10px 10px 0 0;
-      box-shadow: 0 10px 35px -10px grey;
-      .orderLogo {
-        transform: scale(0.5);
-        top: -50px;
-        position: absolute;
-        left: -50px; 
-      }
-      .userBox {
-        display: flex;
-        flex-direction: row;
-        margin-left: auto;
-        width: auto;
-      }
-      .icon {
-        font-size: ${theme.fonts.P2};
-        color: ${theme.colors.greyMedium};
-        height: 35px;
-        width: 35px;
-        margin-right: 10px;
-      }
-    }
   }
 
     @media screen and (min-width: 1000px) {
