066a2c7264b67ccdea48f8e4c92df02fd4c7988b
stephane.malho <stepahen.malho@gmail.com>
2023-04-12 16:27:41 +0200

    ✨ F08:  Admin > Add inputs layout

--- src/components/pages/order/main-content/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/main-content/Admin/AdminPanel/AddForm.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 export default function AddForm() {
   return (
-    <div>AddProduct</div>
-  )
+    <AddFormStyled>
+      <div className="image-previous">Image</div>
+      <div className="inputs-field">
+        <input type="text" value="Nom"/>
+        <input type="text" value="url"/>
+        <input type="text" value="prix"/>
+      </div>
+      <div className="submit-button">button</div>
+    </AddFormStyled>
+  );
 }
+
+const AddFormStyled = styled.div`
+  border: 1px solid black;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: repeat(4, 1fr);
+  height: 100%;
+  width: 70%;
+
+  .image-previous {
+    background: red;
+    grid-area: 1/1/4/2;
+  }
+  .inputs-field {
+    background: blue;
+    grid-area: 1/2/4/3;
+
+    display: grid;
+  }
+  .submit-button {
+    background: green;
+    grid-area: 4/-2/-1/-1;
+  }
+`;
