71537fa2610b9b83dcf3dd61f4ccd81457639444
stephane.malho <stepahen.malho@gmail.com>
2023-05-21 23:18:05 +0200

    ♻️ F12 maths.js > add NAN ?

--- src/utils/maths.js
+++ src/utils/maths.js
@@ -22,6 +22,7 @@ export function replaceFrenchCommaWithDot(price) {
 export function calculateSumToPay(basket, menu) {
   return basket.reduce((total, basketProduct) => {
     const menuProduct = findObjectById(basketProduct.id, menu)
+    if (isNaN(menuProduct.price)) return total;
     total += menuProduct.price * basketProduct.quantity
     return total
   }, 0)
