aafbfab92e8e3b9045f759d6ac0493ffb020d0d8
stephane.malho <stepahen.malho@gmail.com>
2023-07-06 10:18:07 +0200

    ♻️  F14: imagePreview > rename container

--- src/components/pages/order/main-content/Admin/AdminPanel/ImagePreview.jsx
+++ src/components/pages/order/main-content/Admin/AdminPanel/ImagePreview.jsx
@@ -2,17 +2,16 @@ import React from "react";
 import { ImagePreviewStyled } from "../../../../../../styled";
 import { imageState } from "../../../../../../utils/variables";
 
-export default function ImagePreview({ title, imageSource}) {
-
+export default function ImagePreview({ title, imageSource }) {
   return (
-    <ImagePreviewStyled >
-        {imageSource ? (
-          <div className="image-box">
-            <img src={imageSource} alt={title} />
-          </div>
-        ) : (
-          <div className="previews-box">{imageState.empty}</div>
-        )}
-      </ImagePreviewStyled>
-  )
+    <ImagePreviewStyled>
+      {imageSource ? (
+        <div className="image-box">
+          <img src={imageSource} alt={title} />
+        </div>
+      ) : (
+        <div className="preview-container">{imageState.empty}</div>
+      )}
+    </ImagePreviewStyled>
+  );
 }
--- src/styled/uiElementStyled/imagePreviewStyled.js
+++ src/styled/uiElementStyled/imagePreviewStyled.js
@@ -1,7 +1,28 @@
 import styled from "styled-components";
 import { theme } from "../../assets/theme";
 
-const ImagePreviewStyled = styled.div`
+// const previewAnimation = css`
+//   .preview-animation-enter {
+//     ${theme.animations.slideInRight.from};
+//     ${theme.animations.fadeIn.from};
+//   }
+//   .preview-animation-enter-active {
+//     ${theme.animations.transition.medium};
+//     ${theme.animations.slideInRight.to};
+//     ${theme.animations.fadeIn.to};
+//   }
+//   .preview-animation-exit {
+//     ${theme.animations.fadeOut.from};
+//   }
+//   .preview-animation-exit-active {
+//     ${theme.animations.transition.medium};
+//     ${theme.animations.fadeOut.to};
+//     ${theme.animations.slideOutLeft.to};
+//   }
+
+// `
+
+const ImagePreviewStyled = styled("div")`
   font-family: "open sans", sans-serif;
   color: ${theme.colors.greyMedium};
   grid-area: 1/1/4/2;
@@ -12,7 +33,7 @@ const ImagePreviewStyled = styled.div`
   padding: 20px 20px 0 50px;
   height: 130px;
   width: 130px;
-  .previews-box {
+  .preview-container {
     border: 1px solid ${theme.colors.greyLight};
     border-radius: ${theme.borderRadius.round};
     width: 100%;
@@ -29,7 +50,7 @@ const ImagePreviewStyled = styled.div`
     img {
       width: 100px;
       height: 100px;
-      object-fit: contain;
+      object-fit: contain; 
     }
   }
 `;
